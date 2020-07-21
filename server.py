2f0290eb7c9c9da5934ae04471a6c19dae1f4fe2
Ankit Salian <ankitsalian9@gmail.com>
2020-07-21 08:12:33 +0530

    Added the method to store user information in a csv file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,11 +12,16 @@ def write_to_file(data):
     with open('database.txt', 'a') as user_data:
         user_data.write(str(data) + '\n')
 
+def write_to_csv(data):
+    with open('database.csv', 'a') as user_data:
+        csv_writer = csv.writer(user_data, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([data['email'], data['subject'], data['message']])
+
 @app.route('/submit_form', methods= ['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        write_to_file(data)
+        write_to_csv(data)
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong. Try Again!!!'
