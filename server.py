3e6ebfdc4666b4b00869981e178ecd847b5e8f1f
Ankit Salian <ankitsalian9@gmail.com>
2020-07-21 07:44:36 +0530

    Added the method to store user information in a text file

--- server.py
+++ server.py
@@ -7,10 +7,15 @@ def render_page(page=None):
     rendered_page = 'index.html' if page is None else page
     return render_template(f'./{rendered_page}')
 
+def write_to_file(data):
+    with open('database.txt', 'a') as user_data:
+        user_data.write(str(data) + '\n')
+
 @app.route('/submit_form', methods= ['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'Something went wrong. Try Again!!!'
