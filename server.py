ba51d83c8fcea71a67b37dc17d1e51cb499e6e0b
Ankit Salian <ankitsalian9@gmail.com>
2020-07-21 07:20:38 +0530

    Created a form submit method to store the user data and configured a thank you page

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
-@app.route('/<string:page>')
 @app.route('/')
+@app.route('/<string:page>')
 def render_page(page=None):
     rendered_page = 'index.html' if page is None else page
     return render_template(f'./{rendered_page}')
+
+@app.route('/submit_form', methods= ['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong. Try Again!!!'
